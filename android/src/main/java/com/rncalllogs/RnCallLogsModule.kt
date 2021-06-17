60fde9d0877df0a338f960cc5911bdcd43ec72bc
sajanthomas01 <sajanthomas93@gmail.com>
2021-06-18 00:44:44 +0530

    feat: initial push

--- android/src/main/java/com/rncalllogs/RnCallLogsModule.kt
+++ android/src/main/java/com/rncalllogs/RnCallLogsModule.kt
@@ -1,24 +1,112 @@
 package com.rncalllogs
 
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.provider.CallLog
+import com.facebook.react.bridge.*
+import java.lang.Exception
 
-class RnCallLogsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return "RnCallLogs"
+class RnCallLogsModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+  override fun getName(): String {
+    return "RnCallLogs"
+  }
+
+  private val projection = listOf<String>(CallLog.Calls._ID, CallLog.Calls.NUMBER, CallLog.Calls.TYPE, CallLog.Calls.DURATION,
+    CallLog.Calls.DATE, CallLog.Calls.COUNTRY_ISO, CallLog.Calls.CACHED_NAME)
+
+  private val startEpoch: Int = 1606326809;
+  private val stopEpoch: Int = 1607622809;
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun multiply(a: Int, b: Int, promise: Promise) {
+
+    promise.resolve(a * b)
+
+  }
+
+  @ReactMethod
+  fun getAllLogs(limit: Int = 0, skip: Int = 0, promise: Promise) {
+    fetchCallLogs(promise);
+  }
+
+  @ReactMethod
+  fun getOutgoingLogs(limit: Int = 0, skip: Int = 0, promise: Promise) {
+
+  }
+
+  @ReactMethod
+  fun getIncomingLogs(limit: Int = 0, skip: Int = 0, promise: Promise) {
+
+  }
+
+  @ReactMethod
+  fun getMissedLogs(limit: Int = 0, skip: Int = 0, promise: Promise) {
+
+  }
+
+  @ReactMethod
+  fun getNotConnectedLogs(limit: Int = 0, skip: Int = 0, promise: Promise) {
+
+  }
+
+  private fun callTypeMaker(typeId: Int): String {
+    return when(typeId){
+      CallLog.Calls.INCOMING_TYPE -> "INCOMING";
+      CallLog.Calls.OUTGOING_TYPE -> "OUTGOING";
+      CallLog.Calls.MISSED_TYPE -> "MISSED";
+      CallLog.Calls.VOICEMAIL_TYPE -> "VOICEMAIL";
+      CallLog.Calls.REJECTED_TYPE -> "REJECTED";
+      CallLog.Calls.BLOCKED_TYPE -> "BLOCKED";
+      CallLog.Calls.ANSWERED_EXTERNALLY_TYPE -> "EXTERNAL";
+      else -> ""
     }
+  }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
+  private fun fetchCallLogs(promise: Promise) {
+    try {
+      val result = Arguments.createArray()
+
+      val selectionQuery = "${CallLog.Calls.DATE} BETWEEN $startEpoch AND $stopEpoch";
+      val cursor = reactContext.contentResolver.query(CallLog.Calls.CONTENT_URI, projection.toTypedArray(), selectionQuery, projection.toTypedArray(), "DESC")
+      when (cursor?.count) {
+        null -> {
+          promise.reject("Error");
+        }
+        0 -> {
+          promise.resolve(result);
+        }
+        else -> {
+          cursor?.apply {
+            val nameIndex = getColumnIndex(CallLog.Calls.CACHED_NAME);
+            val numberIndex = getColumnIndex(CallLog.Calls.NUMBER);
+            val typeIndex = getColumnIndex(CallLog.Calls.TYPE);
+            val dateIndex = getColumnIndex(CallLog.Calls.DATE);
+            val durationIndex = getColumnIndex(CallLog.Calls.DURATION);
+            val countryIndex = getColumnIndex(CallLog.Calls.COUNTRY_ISO);
+
+            while (moveToNext()) {
+
+              val logData = Arguments.createMap()
+
+              logData.putString("name", getString(nameIndex));
+              logData.putString("number", getString(numberIndex));
+              logData.putString("date", getString(dateIndex));
+              logData.putString("duration", getString(durationIndex));
+              logData.putString("country", getString(countryIndex));
+              logData.putString("type", callTypeMaker(getInt(typeIndex)));
+              result.pushMap(logData);
+              // end of while loop
+            }
+            cursor.close();
+            promise.resolve(result)
+          }
+          promise.resolve(result)
+        }
+      }
+    } catch (e: Exception) {
+      promise.reject("Error")
     }
-
-    
+  }
 }
